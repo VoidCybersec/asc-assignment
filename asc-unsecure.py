c11b3d3a48a24829775ee9cfcff3032546e669cc
Thigendra <void.hacks@outlook.com>
2023-01-15 04:28:41 +0800

    Revert "secure unsecure scripts for atiqah"
    This reverts commit 41893309c71dc35e098251ab2bdbc2ad61eb644f.

--- asc-unsecure.py
+++ asc-unsecure.py
@@ -1,113 +1,143 @@
 import subprocess
 import mysql.connector
+import time
 
-db = mysql.connector.connect(host="localhost", user="root", password="1101", database="asc_unsecure2")
-cursor = db.cursor()
-
-def bash_clear():
+def bashclear():
     subprocess.run("clear")
 
-def add_dish():
-    bash_clear()
-    dish_name = input("Enter the name of the dish          : ")
-    dish_type = input("Enter the type of the dish          : ")
-    dish_origin = input("Enter the the origin of the dish   : ")
+def addbook():
+    bashclear()
+    book_name = input("Enter book name  : ")
+    book_author = input("Enter book author : ")
+    book_year = input("Enter book year   : ")
+
+    mysqldb = mysql.connector.connect(host="localhost", user="root", password="1101", database="asc_unsecure")
+    cursor = mysqldb.cursor()
     
-    cursor.execute(f"INSERT INTO dishes (dish_name, dish_type, dish_origin) VALUES ('{dish_name}', '{dish_type}', '{dish_origin}');")
-    db.commit()
+    cursor.execute(f"insert into books (bname, bauthor, year) values ('{book_name}', '{book_author}', '{book_year}');")
+    mysqldb.commit()
+    mysqldb.close()
 
-    admin_dashboard()    
+    adminpage()    
 
 
-def view_dish_admin():
-    bash_clear()
-    print(" ________________________________________________________")
-    print("|                                                        |")
-    print("| These are the yummy dishes found in the system! Enjoy! |")
-    print("|________________________________________________________|")
+def viewbookadmin():
+    bashclear()
+    print(" _________________________________________________")
+    print("|                                                 |")
+    print("| These are the books found in the system! Enjoy! |")
+    print("|_________________________________________________|")
     
-    db = mysql.connector.connect(host="localhost", user="root", password="1101", database="asc_unsecure2")
-    cursor = db.cursor()
-    cursor.execute(f"SELECT * FROM dishes;")
-    all_dishes = cursor.fetchall()
+    mysqldb = mysql.connector.connect(host="localhost", user="root", password="1101", database="asc_unsecure")
+    cursor = mysqldb.cursor()
+    
+    cursor.execute(f"select * from books;")
+    all_books = cursor.fetchall()
 
-    for dish in all_dishes:
-        print (f"{dish[1]}, a {dish[2]} type dish, originating from {dish[3]}")
+    for book in all_books:
+        print (f"{book[1]}, by {book[2]} from the year {book[3]}")
 
     while True:
-        choice = input("\nBack to main page? (y/n)")
+        choice = input("Back to main page? (y/n)")
         match choice:
             case "y":
-                admin_dashboard()
+                adminpage()
                 break
             case "n":
-                view_dish_admin()
+                viewbookadmin()
                 break
             case _:
                 print("\nInvalid choice! Pick again:")
 
-    admin_dashboard()
+    adminpage()
 
 
 
-def admin_dashboard():
-    bash_clear()
+def adminpage():
+    bashclear()
     print(" _____________________________________________________")
     print("|                                                     |")
-    print("| Welcome to the Dish Managment System Administrator! |")
+    print("| Welcome to the Book Managment System Administrator! |")
     print("|_____________________________________________________|")
-    print("\n1. Add Dish\n2. View Dishes\n3. Exit")
+    print("\n1. Add Book\n2. View Books\n3. Exit")
 
     while True:
         choice = input("\nChoice: ")
         match choice:
             case "1":
-                add_dish()
+                addbook()
                 break
             case "2":
-                view_dish_admin()
+                viewbookadmin()
                 break
             case "3":
                 exit()
             case _:
                 print("Invalid choice! Pick again: ")
 
-def view_dish_user():
-    bash_clear()
-    print(" __________________________________________________")
-    print("|                                                  |")
-    print("| These are the dishes found in the system! Enjoy! |")
-    print("|__________________________________________________|")
+def viewbookuser():
+    bashclear()
+    print(" _________________________________________________")
+    print("|                                                 |")
+    print("| These are the books found in the system! Enjoy! |")
+    print("|_________________________________________________|")
     
-    cursor.execute(f"SELECT * FROM dishes;")
-    all_dishes = cursor.fetchall()
+    mysqldb = mysql.connector.connect(host="localhost", user="root", password="1101", database="asc_unsecure")
+    cursor = mysqldb.cursor()
+    
+    cursor.execute(f"select * from books;")
+    all_books = cursor.fetchall()
 
-    for dish in all_dishes:
-        print (f"{dish[1]}, by {dish[2]} from the year {dish[3]}")
+    for book in all_books:
+        print (f"{book[1]}, by {book[2]} from the year {book[3]}")
 
     while True:
         choice = input("Back to main page? (y/n)")
         match choice:
             case "y":
-                user_dashboard()
+                userpage()
             case "n":
-                view_dish_user()
+                viewbookuser()
             case _:
                 print("\nInvalid choice! Pick again:")
 
-def user_dashboard():
-    bash_clear()
-    print(" ____________________________________________")
-    print("|                                            |")
-    print("| Welcome to the Dish Managment System User! |")
-    print("|____________________________________________|")
-    print("\n1. View Dishes\n2. Exit")
+def adminpage():
+    bashclear()
+    print(" _____________________________________________________")
+    print("|                                                     |")
+    print("| Welcome to the Book Managment System Administrator! |")
+    print("|_____________________________________________________|")
+    print("\n1. Add Book\n2. View Books\n3. Exit")
 
     while True:
         choice = input("\nChoice: ")
         match choice:
             case "1":
-                view_dish_user()
+                addbook()
+                break
+            case "2":
+                viewbookadmin()
+                break
+            case "3":
+                exit()
+            case _:
+                print("Invalid choice! Pick again: ")
+
+
+
+def userpage():
+    bashclear()
+    print(" ____________________________________________")
+    print("|                                            |")
+    print("| Welcome to the Book Managment System User! |")
+    print("|____________________________________________|")
+    print("\n1. View Books\n2. Exit")
+
+    while True:
+        choice = input("\nChoice: ")
+        match choice:
+            case "1":
+                viewbookuser()
             case "2":
                 exit()
             case _:
@@ -115,41 +145,49 @@ def user_dashboard():
 
 def login():
 
-    in_user_name = input("Username: ")
-    in_user_password = input("Password: ")
+    input_username = input("Username: ")
+    input_password = input("Password: ")
     print("\n")
 
-    cursor.execute(f"SELECT user_name FROM credentials WHERE user_name='{in_user_name}';")
+    mysqldb = mysql.connector.connect(host="localhost", user="root", password="1101", database="asc_unsecure")
+    cursor = mysqldb.cursor()
+
+    cursor.execute(f"select username from cred where username='{input_username}';")
     db_out_un = cursor.fetchall()
 
     if len(db_out_un) == 0:
         print("Incorrect username or password!")
+        mysqldb.close()
     else:
         db_username = db_out_un[0][0]
 
-        cursor.execute(f"SELECT user_password FROM credentials WHERE user_name='{db_username}';")
+        cursor.execute(f"select password from cred where username='{db_username}';")
         db_out_pass = cursor.fetchall()
 
         if len(db_out_pass) == 0:
             print("Incorrect usernmae or password!")
+            mysqldb.close()
         else:
             db_password = db_out_pass[0][0]
 
-            if (in_user_name == db_username) and (in_user_password == db_password):
-                if (in_user_name == "admin"):
-                    admin_dashboard()
+            if (input_username == db_username) and (input_password == db_password):
+                if (input_username == "admin"):
+                    adminpage()
+                    mysqldb.close()
                 else:
-                    user_dashboard()
+                    userpage()
+                    mysqldb.close()
             else:
                 print("Incorrect usernmae or password!")
+                mysqldb.close()
         
-def begin():
-    bash_clear()
+def main():
+    bashclear()
     print(" _________________________________________________________________")
     print("|                                                                 |")
-    print("| Welcome to the Dish Managment System! Please log in to proceed! |")
+    print("| Welcome to the Book Managment System! Please log in to proceed! |")
     print("|_________________________________________________________________|")
     
     login()
 
-begin()
+main()
